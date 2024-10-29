794e4523fd0868a1eaf6f7d132919f4a3a605128
thihaaung-plusimpact <thi.ha.aung@plus-impact.co>
2024-10-29 13:34:08 +0630

    Starter

---
+++ src/bloom-mail/resources/js/vuetify.js
@@ -0,0 +1,7 @@
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; // Include the Vuetify styles
+import { createApp } from 'vue';
+
+const vuetify = createVuetify(); // Create a Vuetify instance
+
+export default vuetify;
